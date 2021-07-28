cf60ad271077b14855600fd8e2f48867f9c08a73
wendy <rdbaek@lunasoft.co.kr>
2021-07-28 17:57:06 +0900

    spec.ts파일로 유닛테스트 진행하기

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { exception } from 'console';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -14,5 +16,42 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe("getAll", () => {
+
+    it('should return an array', () =>{
+      //service에 접근 가능
+      const result = service.getAll();
+      //타입이 배열인 지 확인해볼 것
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe("getOne", () => {
+
+    it('should return a movie', () =>{
+       //movie 생성
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      //service에 접근 가능
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should throw 404 error', () =>{
+      //try catch로 404에러 뜨는 지 확인
+      try{
+        service.getOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('movie with id 999 Not Found');
+      }
+    });
+
   });
 });
